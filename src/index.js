6eb6d8bd3e7bb5460e59f3dbfaeb2224bd8acc88
manyeya <siemakhutso@gmail.com>
2020-01-30 20:31:32 +0200

    made a few tweaks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles/main.scss'
+
+const email = document.getElementById('email');
+const errorMsg = document.querySelector('.error');
+const errorIcon= document.querySelector('#errorIcon');
+email.addEventListener('input', () => {
+    if (email.validity.typeMismatch) {
+        errorMsg.style.display = 'block';
+        errorIcon.style.display = 'block';
+    } else {
+        errorMsg.style.display = 'none';
+        errorIcon.style.display = 'none';
+    }
+})
